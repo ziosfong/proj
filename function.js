3938741a2d2f57d06041c483290b7d9b48ce8bb8
ziosfong <hkgsugita@gmail.com>
2016-11-20 07:37:21 +0800

    Add files via upload

--- function.js
+++ function.js
@@ -130,7 +130,7 @@ function price(){
 	
 	var tdv = year.toString() + month.toString();
 		document.getElementById("SFAv").value = SFA ;
-
+document.getElementById("direct_facing").value = "no" ;
 		
 		
 	
@@ -486,7 +486,7 @@ switch (phasev){
 	//window.alert("age");
 	//price
 	
-	e_p =  -0.081357 * age + 0.002290 * age * age + 0.003177 * SFA + 2.05* 0.0000001* SFA * SFA + 0.010539 * fl * fl * fl + 0.041815 * DirS + 0.022967 * LU + 0.427346 * cp + TD - 8.35 * 0.00001  ;
+	e_p =  -0.081357 * age + 0.002290 * age * age + 0.003177 * SFA + 2.05* 0.0000001* SFA * SFA + 0.010539 * fl  + 0.041815 * DirS + 0.022967 * LU + 0.427346 * cp + TD - 8.35 * 0.00001* fl * fl  ;
 	
 	//print out
 	
