78aa26c67e6268392a226375bb70befc3c3b75b2
ziosfong <hkgsugita@gmail.com>
2016-11-20 19:43:54 +0800

    Add files via upload

--- function.js
+++ function.js
@@ -100,16 +100,14 @@ function agenum(){
 }
 
 function blknum(){
-	  phasev = parseInt(document.getElementById("phase").value);
-	  blockv = parseInt(document.getElementById("block").value);
 	$("#phase").change(function(){
   switch (parseInt($(this).val())){
 
         case 0: 
-      $("block option").remove();
+      $("#block option").remove();
       break;
         case 1: 
-      $("block option").remove();
+      $("#block option").remove();
       var array = [ "1", "2", "3", "4", "5","6","7","8","9","10","11","12","13","14" ];
       //利用each遍歷array中的值並將每個值新增到Select中
       $.each(array, function(i, val) {
